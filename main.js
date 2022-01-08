9276a184e783724a2a5008f6d04ff2feacf9d6ae
June Kim <godlyjune@gmail.com>
2022-01-08 19:36:21 +0900

    Fix menu to hide after clicked

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const onMenuClick = () => {
   const menu = document.getElementById("menu");
-  const newClass = menu.className === "none" ? "clicked" : "none";
+  const newClass = menu.className === "hide" ? "show" : "hide";
   menu.className = newClass;
+};
+
+const onMenuItemClick = () => {
+  const menu = document.getElementById("menu");
+  menu.className = "hide";
 };
