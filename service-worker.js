ee6d2e7274ed61546122b0019e78d2e4c589937e
June Kim <godlyjune@gmail.com>
2022-05-19 10:56:19 +0900

    Upgrade Font Awesome to 6.1.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "1.0.9";
+const VERSION = "1.0.10";
 const STATIC_CACHE = `static-v${VERSION}`;
 const DYNAMIC_CACHE = `dynamic-v${VERSION}`;
 const installList = [
@@ -7,7 +7,7 @@ const installList = [
   "/style-media.css",
   "/main.js",
   "/py.webp",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
   "/guilty_dog.jpg",
   "/offline.html",
 ];
